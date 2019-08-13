1a2ac0e7029942ebbdb4f557ee773b2b147c1b11
DineshMohan0001 <dineshmohan0001@gmail.com>
2019-08-13 14:50:01 +0530

    new

--- newsample.py
+++ newsample.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 print("UPDATE")
 this is written in the branch line 2
 this is the update to the file 
 
+=======
+print("new sample")
+let us check for the auto update in the process
+>>>>>>> new
