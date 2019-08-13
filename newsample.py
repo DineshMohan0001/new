9f4a66c0e384b43a8972cb3674183af81b94fc78
DineshMohan0001 <dineshmohan0001@gmail.com>
2019-08-13 14:57:59 +0530

    123

--- newsample.py
+++ newsample.py
@@ -2,8 +2,12 @@
 print("UPDATE")
 this is written in the branch line 2
 this is the update to the file 
+<<<<<<< HEAD
 
 =======
 print("new sample")
 let us check for the auto update in the process
 >>>>>>> new
+=======
+thsi is the rebase
+>>>>>>> 123
